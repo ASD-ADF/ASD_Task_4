834467e4d009f009e0d165f86ca162576d7535d1
Muhammad Yuslan Abubakar <yuslan.black@gmail.com>
2015-10-01 21:38:57 +0700

    Update Player.h

--- player.h
+++ player.h
@@ -3,7 +3,7 @@
 
 #include "list.h"
 
-void inputNewSong(infotype &);
+void inputNewSong(List &L, infotype &);
 // input new song into the list
 // procedure receive input from user
 
@@ -19,18 +19,17 @@ void playNext(address &P);
 void playPrev(address &P);
 // play the previous song from element P
 
-void shuffleList(List &);
+void shuffleList(List &L);
 // shuffle the song list
 
-void sortList(List &, int condition);
+void sortList(List &L, int condition);
 // sort song List by some condition
 // example if condition = 1, then sort by ID
 //          if condition = 2, then sort by name
 
-void playRepeat(List &, int n);
+void playRepeat(List &L, int n);
 // play the song list from the first song
 // and repeat the list n times
-
 
 
 #endif // PLAYER_H_INCLUDED
