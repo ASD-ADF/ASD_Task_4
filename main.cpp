3c73b389e11d0a8d8addf085d01e3d2e5aabd76a
Muhammad Arif Kurniawan <muharifkur@gmail.com>
2015-10-01 22:12:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "list.h"
 #include "player.h"
 #include <conio.h>
-
+#include <stdlib.h>
 using namespace std;
 
 List L;
-address P;
+int j;
+address P,s;
 infotype x;
-
 void menu();
 void displayMenu();
 void runMenu(int menu);
@@ -34,46 +34,95 @@ int main() {
     return 0;
 }
 
-void menu() {
+void menu()
+{
     int pil;
-    do {
+    do
+    {
         displayMenu();
         cin>>pil;
         runMenu(pil);
-    } while (pil!=5);
+    }
+    while (pil!=11);
 }
 
-void displayMenu() {
-    cout<<"1. input new "<<endl
-        <<"2. view list"<<endl
-        <<"3. play first song"<<endl
-        <<"4. play next "<<endl
-        <<"5. exit"<<endl;
+void displayMenu()
+{
+    system("cls");
+    cout<<"~~~~~~~~~~~~~~~~~~MENU~~~~~~~~~~~~~~~~~"<<endl
+        <<"1. Input New "<<endl
+        <<"2. View List"<<endl
+        <<"3. Search Song by Name"<<endl
+        <<"4. Play First Song"<<endl
+        <<"5. Play Next "<<endl
+        <<"6. Play Prev"<<endl
+        <<"7. Play Again the Last Played"<<endl
+        <<"8. Shuffle Song List"<<endl
+        <<"9. Sort the Song List"<<endl
+        <<"10. Repeat All SOngs"<<endl
+        <<"11. Exit"<<endl;
     cout<<"choose menu : ";
 }
 
-void runMenu(int menu) {
-    switch(menu) {
+void runMenu(int menu)
+{
+    switch(menu)
+    {
     case 1 :
-        cout<<"input new song : "<<endl;
-        inputNewSong(x);
-        P = alokasi(x);
-        insertFirst(L,P);
+        system("cls");
+       
         break;
     case 2:
-        printInfo(L);
+        system("cls");
+       
         break;
     case 3 :
-        P = First(L);
-        playSong(P);
+        system("cls");
+        
         break;
-    case 4:
-        playNext(P);
+    case 4 :
+        system("cls");
+       
         break;
     case 5:
-        cout<<"thank you"<<endl;
+        system("cls");
+        
+        break;
+    case 6:
+        system("cls");
+       
+        break;
+    case 7:
+        system("cls");
+      
+        break;
+    case 8 :
+        system("cls");
+        shuffleList(L);
+        getch();
+        break;
+    case 9:
+        system("cls");
+        cout<<"Shorting berdasarkan : "<<endl
+            <<"1. Berdasarkan ID "<<endl
+            <<"2. Berdasarkan Nama "<<endl
+            <<"Masukkan Nomor Kondisi yang dipilih : ";
+        cin>>j;
+        sortList(L,j);
+        getch();
+        break;
+    case 10:
+        system("cls");
+        cout<<"Masukkan jumlah perulangan : "; cin>>j;
+        playRepeat(L,j);
+        getch();
+        break;
+    case 11:
+        system("cls");
+        cout<<"Thank You"<<endl;
         break;
     default :
-        cout<<"wrong input"<<endl;
+        system("cls");
+        cout<<"Wrong Input"<<endl;
     }
 }
