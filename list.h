dcc81bd3a0ccaa973fb8d2de8a15556e4aefc527
Muhammad Arif Kurniawan <muharifkur@gmail.com>
2015-10-01 22:08:21 +0700

    Update list.h

--- list.h
+++ list.h
@@ -5,10 +5,14 @@
 #include <windows.h>
 
 #define First(L) L.first
+#define Last(L) L.last
 #define Next(P) P->next
+#define Prev(P) P->prev
 #define Info(P) P->info
 
+
 using namespace std;
+
 
 struct infotype
 {
@@ -17,32 +21,23 @@ struct infotype
     string location;
 };
 
+
 typedef struct elemenList *address;
 struct elemenList
 {
     infotype info;
-    address next;
+    address next,prev;
 };
+
 
 struct List
 {
-    address first;
+    address first,last;
 };
 
-void createList(List &);
 
-address alokasi(infotype );
-void dealokasi(address &);
-
-void insertFirst(List &, address );
-void insertLast(List &, address );
-void insertAfter(List &, address , address );
-
-void deleteFirst(List &, address &);
-void deleteLast(List &, address &);
-void deleteAfter(List &, address &, address &);
 
 address findElm(List, infotype );
-
+int countElm(List L);
 
 #endif // LIST_H_INCLUDED
