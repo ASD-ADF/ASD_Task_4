3f05d89424600bdff611481857c8c4230047de48
Muhammad Yuslan Abubakar <yuslan.black@gmail.com>
2015-10-01 21:34:54 +0700

    Update Terbaru

--- main.cpp
+++ main.cpp
@@ -2,25 +2,29 @@
 #include "list.h"
 #include "player.h"
 #include <conio.h>
+#include <cstdlib>
 
 using namespace std;
 
 List L;
 address P;
+address Prec;
 infotype x;
+int condition;
+int number;
 
 void menu();
 void displayMenu();
 void runMenu(int menu);
 
 int main() {
-    cout << "Hello world!" << endl;
+    cout << "WAV Music Player Kelompok 2" << endl;
 
     createList(L);
     // example of data initialization
-    x.ID = 1;
+    x.ID = 4;
     x.location = "";
-    x.name = "clapping.wav";
+    x.name = "airpump2.wav";
     P = alokasi(x);
     insertFirst(L,P);
 
@@ -28,7 +32,25 @@ int main() {
     x.location = "";
     x.name = "airpump2.wav";
     P = alokasi(x);
-    insertFirst(L,P);
+    insertLast(L,P);
+
+    x.ID = 3;
+    x.location = "";
+    x.name = "airpump2.wav";
+    P = alokasi(x);
+    insertLast(L,P);
+
+    x.ID = 1;
+    x.location = "";
+    x.name = "clapping.wav";
+    P = alokasi(x);
+    insertLast(L,P);
+
+    x.ID = 0;
+    x.location = "";
+    x.name = "asdasdclapping.wav";
+    P = alokasi(x);
+    insertLast(L,P);
 
     menu();
     return 0;
@@ -37,43 +59,119 @@ int main() {
 void menu() {
     int pil;
     do {
+        cout<<endl;
         displayMenu();
-        cin>>pil;
+        cin>>pil; cin.ignore();
         runMenu(pil);
-    } while (pil!=5);
+    } while (pil != 10);
 }
 
 void displayMenu() {
-    cout<<"1. input new "<<endl
-        <<"2. view list"<<endl
-        <<"3. play first song"<<endl
-        <<"4. play next "<<endl
-        <<"5. exit"<<endl;
-    cout<<"choose menu : ";
+    system("CLS");
+    cout<<"1. Input New "<<endl
+        <<"2. Delete Element"<<endl
+        <<"3. Sort List"<<endl
+        <<"4. View List"<<endl
+        <<"5. Play First Song"<<endl
+        <<"6. Play Next "<<endl
+        <<"7. Play Prev"<<endl
+        <<"8. Shuffle"<<endl
+        <<"9. Play Repeat"<<endl
+        <<"10. Exit"<<endl;
+    cout<<"Choose menu : ";
 }
 
 void runMenu(int menu) {
     switch(menu) {
     case 1 :
         cout<<"input new song : "<<endl;
-        inputNewSong(x);
+        inputNewSong(L,x);
         P = alokasi(x);
-        insertFirst(L,P);
+        cout <<"Pilih Posisi    : 1. First"<<endl
+             <<"                  2. After"<<endl
+             <<"                  3. Last"<<endl
+             <<"Masukkan Posisi : ";cin>>number;
+        if (number == 1) {
+            insertFirst(L,P);
+            cout << "Lagu Berhasil Diinput";
+        }
+        else if (number == 2) {
+            address R = new elemenList;
+            cout << "Masukkan ID Element Sebelum Element Baru Yang Akan Diinput : ";
+            cin >> R->info.ID;
+            insertAfter(L,P,R);
+            cout << "Lagu Berhasil Diinput";
+        }
+        else if (number == 3) {
+            insertLast(L,P);
+            cout << "Lagu Berhasil Diinput";
+        }
+        else {
+            cout << "Menu Salah"<<endl;
+        }
+        getch();
         break;
-    case 2:
-        printInfo(L);
+    case 2 :
+        cout <<"Pilih Posisi    : 1. First"<<endl
+             <<"                  2. After"<<endl
+             <<"                  3. Last"<<endl
+             <<"Masukkan Posisi : ";cin>>number;
+        if (number == 1) {
+            deleteFirst(L,P);
+            cout << "Lagu Berhasil Dihapus";
+            getch();
+        }
+        else if (number == 2) {
+            address R = new elemenList;
+            cout << "Masukkan ID Element Sebelum Element Yang Akan Dihapus : ";
+            cin >> R->info.ID;
+            deleteAfter(L,P,R);
+            cout << "Lagu Berhasil Dihapus";
+            getch();
+        }
+        else if (number == 3) {
+            deleteLast(L,P);
+            cout << "Lagu Berhasil Dihapus";
+            getch();
+        }
+        else {
+            cout <<"Menu Salah"<<endl;
+            getch();
+        }
         break;
     case 3 :
+        cout << "Pilihan Sort : 1. Sort By ID"<<endl
+             << "               2. Sort By Name"<<endl
+             << "Pilihan      : ";cin >> condition;
+        sortList(L,condition);
+        getch();
+        break;
+    case 4 :
+        printInfo(L);
+        getch();
+        break;
+    case 5 :
         P = First(L);
         playSong(P);
         break;
-    case 4:
+    case 6 :
         playNext(P);
         break;
-    case 5:
-        cout<<"thank you"<<endl;
+    case 7 :
+        playPrev(P);
+        break;
+    case 8 :
+        shuffleList(L);
+        break;
+    case 9 :
+        int n;
+        cout << "Masukkan Nilai Repeat-an : ";cin>>n;
+        playRepeat(L,n);
+        break;
+    case 10 :
+        cout<<"Thank You"<<endl;
         break;
     default :
-        cout<<"wrong input"<<endl;
+        cout<<"Wrong Input"<<endl;
     }
 }
