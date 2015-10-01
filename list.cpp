880912b7aa3998e2c57342199a433fb9e4975f64
Muhammad Arif Kurniawan <muharifkur@gmail.com>
2015-10-01 22:07:04 +0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,100 +1,81 @@
 #include <iostream>
 #include "list.h"
 
+
 using namespace std;
+
 
 void createList(List &L)
 {
-    First(L) = NULL;
+    
 }
+
 
 address alokasi(infotype x)
 {
-    address P = new elemenList;
-    Info(P) = x;
-    Next(P) = NULL;
-    return P;
+  
 }
+
 
 void dealokasi(address &P)
 {
-    delete P;
+    
 }
+
 
 void insertFirst(List &L, address P)
 {
-    Next(P) = First(L);
-    First(L) = P;
+    
 }
 
 void insertLast(List &L, address P)
 {
-    if(First(L) == NULL)
-    {
-        insertFirst(L,P);
-    }
-    else
-    {
-        address Q = First(L);
-        while(Next(Q) != NULL)
-        {
-            Q = Next(Q);
-        }
-        Next(Q) = P;
-    }
+  
 }
 void insertAfter(List &L, address P, address Prec)
 {
-    if(First(L) == NULL)
-    {
-        insertFirst(L,P);
-    }
-    else
-    {
-        Next(P) = Next(Prec);
-        Next(Prec) = P;
-    }
+    
 }
 
 void deleteFirst(List &L, address &P)
 {
-    P = First(L);
-    First(L) = Next(P);
-    Next(P) = NULL;
+    
 }
+
 
 void deleteLast(List &L, address &P)
 {
-    if(Next(First(L)) == NULL)
-    {
-        deleteFirst(L,P);
-    }
-    else
-    {
-        address Q = First(L);
-        while(Next(Next(Q)) != NULL)
-        {
-            Q = Next(Q);
-        }
-        P = Next(Q);
-        Next(Q) = NULL;
-    }
+    
 }
 
 void deleteAfter(List &L, address &P, address &Prec)
 {
-    P = Next(Prec);
-    Next(Prec) = Next(P);
-    Next(P) = NULL;
+   
 }
 
-address findElm(List L, infotype x){
+address findElm(List L, infotype x)
+{
     address Q = First(L);
-    while(Q != NULL){
-        if(Info(Q).ID == x.ID){
+    while(Q != NULL)
+    {
+        if(Info(Q).name == x.name)
+        {
             return Q;
         }
         Q = Next(Q);
     }
     return NULL;
+}
+int countElm(List L)
+{
+    int num = 0;
+    if (L.first == NULL) return 0;
+    address Q = First(L);
+    do
+    {
+        num++;
+        Q = Next(Q);
+    }
+    while(Q != L.first);
+    return num;
 }
