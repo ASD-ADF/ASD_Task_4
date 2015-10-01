31655b14eda18d088be0b6a827156e646a175448
Hizas Sabilal Rasyad <hizassr@gmail.com>
2015-10-02 00:44:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,79 +1,81 @@
-#include <iostream>
 #include "list.h"
 #include "player.h"
-#include <conio.h>
+#include <time.h>
 
-using namespace std;
+int main()
+{
+    /*Variable Declaration*/
+    List L;
+    element *p, *music;
+    char choose,subchoose;
+    int condition;
 
-List L;
-address P;
-infotype x;
+    //Seed Random
+    srand(time(NULL));
 
-void menu();
-void displayMenu();
-void runMenu(int menu);
+    /*Main Program*/
+    create_list(L); // List L Head and Tail assigned NULL
+    music = NULL; // Pointer Music assigned NULL
 
-int main() {
-    cout << "Hello world!" << endl;
+    while(choose != '0') // 0 to Exit
+    {
+        subchoose = 'a'; // Reset the value of subchoose
+        system("cls");
+        menu(L,music); //Output Menu
+        choose = _getch();
+        system("cls");
+        switch(choose)
+        {
+            case '1' : /* Insert Menu */
+                {
+                    while(subchoose != '0') // 0  to Exit
+                    {
+                        insert_menu();
+                        subchoose = _getch();
+                        system("cls");
+                        switch(subchoose)
+                        {
+                            case '1' : insert_first(L,p); subchoose = '0'; break;
+                            case '2' : insert_last(L,p); subchoose = '0'; break;
+                            case '3' : insert_after(L); subchoose = '0'; break;
+                            default  : break;
+                        }
+                    }
+                    if (L.head == L.tail)
+                        {
+                            music = L.head;
+                        }
+                        break;
+                }
+            case '2' : /* Delete Menu */
+                {
+                    while(subchoose != '0')
+                    {
+                        delete_menu();
+                        subchoose = _getch();
+                        system("cls");
+                        switch(subchoose)
+                        {
+                            case '1' : delete_first(L,p); subchoose = '0'; break;
+                            case '2' : delete_last(L,p); subchoose = '0'; break;
+                            case '3' : delete_after(L); subchoose = '0'; break;
+                            default  : break;
+                        }
+                    } music = (L).head; break;
 
-    createList(L);
-    // example of data initialization
-    x.ID = 1;
-    x.location = "";
-    x.name = "clapping.wav";
-    P = alokasi(x);
-    insertFirst(L,P);
+                }
+            case '3' : view_music_list(L); break;
+            case '4' : play_song(music); break;
+            case '5' : play_next(music); break;
+            case '6' : play_prev(music); break;
+            case '7' : searching(L,music); break;
+            case '8' : shuffle_list(L); break;
+            case '9' : sort_list(L,condition); break;
+            case '-' : repeat_all_song(L,music); break;
+            default  : break;
 
-    x.ID = 2;
-    x.location = "";
-    x.name = "airpump2.wav";
-    P = alokasi(x);
-    insertFirst(L,P);
-
-    menu();
-    return 0;
-}
-
-void menu() {
-    int pil;
-    do {
-        displayMenu();
-        cin>>pil;
-        runMenu(pil);
-    } while (pil!=5);
-}
-
-void displayMenu() {
-    cout<<"1. input new "<<endl
-        <<"2. view list"<<endl
-        <<"3. play first song"<<endl
-        <<"4. play next "<<endl
-        <<"5. exit"<<endl;
-    cout<<"choose menu : ";
-}
-
-void runMenu(int menu) {
-    switch(menu) {
-    case 1 :
-        cout<<"input new song : "<<endl;
-        inputNewSong(x);
-        P = alokasi(x);
-        insertFirst(L,P);
-        break;
-    case 2:
-        printInfo(L);
-        break;
-    case 3 :
-        P = First(L);
-        playSong(P);
-        break;
-    case 4:
-        playNext(P);
-        break;
-    case 5:
-        cout<<"thank you"<<endl;
-        break;
-    default :
-        cout<<"wrong input"<<endl;
+        }
     }
+    cout<<"Closing HM Player.."<<endl;
+    return 0;
 }
