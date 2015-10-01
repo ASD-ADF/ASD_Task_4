9f1f5254fb97e7a4983ee4de8a41dfd97b79f5ec
Tari Lestari <tarilessstari@gmail.com>
2015-10-01 22:16:00 +0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -6,6 +6,49 @@
 #include "player.h"
 using namespace std;
 
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <windows.h>
+#include "list.h"
+#include "player.h"
+using namespace std;
+
+void input_music (infotype &x)
+{
+    cout<<"=====IMPORT MUSIC====="<<endl;
+    cout<<"Input music name (.wav) : ";
+    cin>>x.name;
+    cout<<"Input music location (write - for default location) : ";
+    cin>>x.location;
+    cout<<"======================"<<endl;
+    if(x.location=="-")
+        x.location="";
+}
+
+void print_info (list L)
+{
+    address Q;
+
+    Q=first(L);
+    if (Q!=NULL)
+    {
+        cout<<"=====VIEW PLAYLIST====="<<endl;
+        cout<<info(Q).id<<" "<<info(Q).name<<endl;
+        Q=next(Q);
+        while(Q!=first(L))
+        {
+            cout<<info(Q).id<<" "<<info(Q).name<<endl;
+            Q=next(Q);
+        }
+    }
+    else
+    {
+        cout<<"=====VIEW PLAYLIST====="<<endl;
+        cout<<"No music"<<endl;
+    }
+}
+
 void play_prev (address &P)
 {
     P=prev(P);
