53a03ee6995fa9c2baef9f647d5071707fa5fb82
yuri liadi <yuriliadi@gmail.com>
2015-10-01 22:36:29 +0700

    update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+void createList(List &L)
+{
+    First(L) = NULL;
+    Last(L) = NULL;
+}
+
+address alokasi(infotype x)
+{
+    address P = new elemenList;
+    Info(P) = x;
+    Next(P) = NULL;
+    P->prev = NULL;
+    return P;
+}
+
+void dealokasi(address &P)
+{
+    delete P;
+}
+
 void insertFirst(List &L, address P)
 {
     if (L.first == NULL) {
@@ -73,4 +98,65 @@ void deleteFirst(List &L, address &P)
         P->prev = NULL;
         dealokasi(P);
     }
+}
+
+void deleteLast(List &L, address &P)
+{
+    if(L.first == NULL)
+    {
+        cout << "List Kosong"<<endl;
+    }
+    else if (L.first->next == NULL)
+    {
+        deleteFirst(L,P);
+    }
+    else {
+        P = L.last;
+        L.last = P->prev;
+        L.last->next = L.first;
+        L.first->prev = L.last;
+        P->prev = NULL;
+        P->next = NULL;
+        dealokasi(P);
+    }
+}
+
+void deleteAfter(List &L, address &P, address &Prec)
+{
+    address S = L.first;
+    for (int i = 1; i <= countElm(L); i++){
+        if (Prec->info.ID == S->info.ID) {
+            break;
+        }
+        S = S->next;
+    }
+    Prec = S;
+
+    P = Prec->next;
+    Prec->next = P->next;
+    P->next = NULL;
+    P->prev = NULL;
+    dealokasi(P);
+}
+
+address findElm(List L, infotype x){
+    address Q = First(L);
+    while(Q != NULL){
+        if(Info(Q).ID == x.ID){
+            return Q;
+        }
+        Q = Next(Q);
+    }
+    return NULL;
+}
+
+int countElm(List L) {
+    int num = 0;
+    if (L.first == NULL) return 0;
+    address Q = First(L);
+    do{
+        num++;
+        Q = Next(Q);
+    }while(Q != L.first);
+    return num;
 }
