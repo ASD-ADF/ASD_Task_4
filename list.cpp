a52c8e1a707f52f12b5b679405816759e9d0386e
Annisa Rizki Pratiwi Wibowo <annisaarizki@yahoo.co.id>
2015-10-02 00:44:13 +0900

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -39,19 +39,58 @@ void insertAfter(List &L, address P, address Prec)
 
 void deleteFirst(List &L, address &P)
 {
-    
+    if(First(L)==NULL)
+    {
+        cout<<"Data Kosong"<<endl;
+    }
+    else
+    {
+        P = First(L);
+        First(L) = Next(P);
+        Next(P) = NULL;
+        Prev(P)=NULL;
+        Next(P)=NULL;
+    }
 }
 
 
 void deleteLast(List &L, address &P)
 {
-    
+    if(First(L)==NULL)
+    {
+        cout<<"Data Kosong"<<endl;
+    }
+    else if(Next(First(L)) == NULL)
+    {
+        deleteFirst(L,P);
+    }
+    else
+    {
+        P = Last(L);
+        Last(L)=Prev(P);
+        Next(Last(L))=First(L);
+        Prev(First(L))=Last(L);
+        Prev(P)=NULL;
+        Next(Last(L))=NULL;
+    }
 }
 
 void deleteAfter(List &L, address &P, address &Prec)
 {
-   
+    address Q = First(L);
+    for (int i = 1; i <= countElm(L); i++){
+        if (Info(Prec).ID == Info(Q).ID) {
+            break;
+        }
+        Q = Q->next;
+    }
+    Prec = Q;
+    P=Next(Prec);
+    Next(Prec) = Next(P);
+    Prev(P)=NULL;
+    Next(P) = NULL;
 }
+
 
 address findElm(List L, infotype x)
 {
