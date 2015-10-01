7bb11433a5d086b4085e6b5a06cde14f36b967a7
friendlysipayung <friendly.1301144001@gmail.com>
2015-10-01 22:16:06 +0700

    player.cpp
    add sortList with countElm

--- player.cpp
+++ player.cpp
@@ -43,3 +43,70 @@ void playPrev(address &P)
     playSong(P);
 }
 
+int countElm(List L)
+{
+    int num = 0;
+    if (L.first == NULL) return 0;
+    address Q = First(L);
+    do
+    {
+        num++;
+        Q = Next(Q);
+    }
+    while(Q != L.first);
+    return num;
+}
+
+void sortList(List &L, int condition)
+{
+    infotype temp;
+    int i,j,k;
+    if (condition == 1)
+    {
+        address P;
+        P = First(L);
+        for (i = 1; i <= countElm(L)-1; i++)
+        {
+            P = First(L);
+            for (j = 1 ; j <= (countElm(L) - i) ; j++)
+            {
+                if (Info(P).ID > Info(Next(P)).ID)
+                {
+                    temp = Info(Next(P));
+                    Info(Next(P)) = Info(P);
+                    Info(P) = temp;
+                    P = Next(P);
+                }
+                else
+                {
+                    P = Next(P);
+                }
+
+            }
+        }
+    }
+    else if (condition == 2)
+    {
+        address P;
+        P = First(L);
+        for (i = 1; i <= countElm(L)-1; i++)
+        {
+            P = First(L);
+            for (j = 1 ; j <= (countElm(L) - i) ; j++)
+            {
+                if (Info(P).name > Info(Next(P)).name)
+                {
+                    temp = Info(Next(P));
+                    Info(Next(P)) = Info(P);
+                    Info(P) = temp;
+                    P = Next(P);
+                }
+                else
+                {
+                    P = Next(P);
+                }
+
+            }
+        }
+    }
+}
