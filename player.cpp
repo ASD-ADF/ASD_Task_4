b215e13f3e2941250e54e7fceff82f9a76e677e3
Adi Nugroho <adi.smarispat@gmail.com>
2015-10-02 09:42:02 +0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -36,4 +36,44 @@ void playNext(address &P){
     P = Next(P);
     playSong(P);
 }
+void sortList(List &L, int condition) {
+    switch(condition) {
+        case 1: {
+            bool blmselesai = true;
+            address tmp;
+            while(blmselesai) {
+                blmselesai=false;
+                address P=Next(First(L));
+                while(Next(P)!=Next(First(L))) {
+                    if(Info(P).ID<Info(Prev(P)).ID) {
+                        address tmp=new elemenList;
+                        Info(tmp)=Info(Prev(P));
+                        Info(Prev(P))=Info(P);
+                        Info(P)=Info(tmp);
+                        blmselesai=true;
+                    }
+                    P=Next(P);
+                }
+            }
+        }
+        case 2: {
+            bool blmselesai = true;
+            address tmp;
+            while(blmselesai) {
+                blmselesai=false;
+                address P=Next(First(L));
+                while(Next(P)!=Next(First(L))) {
+                    if(Info(P).name<Info(Prev(P)).name) {
+                        address tmp=new elemenList;
+                        Info(tmp)=Info(Prev(P));
+                        Info(Prev(P))=Info(P);
+                        Info(P)=Info(tmp);
+                        blmselesai=true;
+                    }
+                    P=Next(P);
+                }
+            }
+        }
+    }
+}
 
