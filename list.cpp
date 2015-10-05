83de333a15411ca81668023252361379e014779c
RizkyFadhillah <fadhillahr89@yahoo.co.id>
2015-10-05 18:56:12 +0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -75,36 +75,71 @@ void insertAfter(List &L, address P, address Prec){
     }
 }
 
+//Procedure DeleteFirst by Rizky Fadhillah
 void deleteFirst(List &L, address &P)
 {
-    P = First(L);
-    First(L) = Next(P);
-    Next(P) = NULL;
+    if(First(L)==NULL) {
+        cout<<"Data Kosong";
+    }
+    else if (First(L)==Last(L)) {
+            P=First(L);
+            Next(P)=NULL;
+            Prev(P)=NULL;
+            First(L)=NULL;
+            Last(L)=NULL;
+    }
+    else{
+        P = First(L);
+        First(L) = Next(P);
+        Next(P) = NULL;
+        Prev(P) = NULL;
+        Prev(First(L))=Last(L);
+        Next(Last(L))=First(L);
+    }
+
 }
 
+//Procedure DeleteLast by Rizky Fadhillah
 void deleteLast(List &L, address &P)
 {
-    if(Next(First(L)) == NULL)
+    if(First(L)==NULL)
     {
-        deleteFirst(L,P);
+        cout<<"Data Kosong"<<endl;
+    }
+    else if(First(L)==Last(L)) {
+        P=Last(L);
+        Next(P)=NULL;
+        Prev(P)=NULL;
+        First(L)=NULL;
+        Last(L)=NULL;
     }
     else
     {
-        address Q = First(L);
-        while(Next(Next(Q)) != NULL)
-        {
-            Q = Next(Q);
-        }
-        P = Next(Q);
-        Next(Q) = NULL;
+        P=Last(L);
+        Last(L)=Prev(Last(L));
+        Prev(P)=NULL;
+        Next(P)=NULL;
+        Prev(First(L))=Last(L);
+        Next(Last(L))=First(L);
     }
 }
 
+//Procedure DeleteLast by Rizky Fadhillah
 void deleteAfter(List &L, address &P, address &Prec)
 {
-    P = Next(Prec);
-    Next(Prec) = Next(P);
-    Next(P) = NULL;
+    if(Prec==NULL) {
+        cout<<"Data Kosong"<<endl;
+    }
+    else if (Prec==First(L)) {
+        deleteFirst(L,P);
+    }
+    else {
+     P = Next(Prec);
+     Next(Prec) = Next(P);
+     Prev(Next(P))=Prec;
+     Next(P) = NULL;
+     Prev(P) = NULL;
+    }
 }
 
 address findElm(List L, infotype x){
