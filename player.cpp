7b95035f31be1390d41717ce4f4c3892e846cd18
raishafiyyan <hafiyyanrais@gmail.com>
2015-10-05 21:10:26 +0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -6,8 +6,18 @@
 
 using namespace std;
 
-void inputNewSong(infotype &x)
+void inputNewSong(infotype &x, List &L)
 {
+    address cek;
+    x.ID = 0;
+    cek = First(L);
+    x.ID++;
+    while (cek != Last(L))
+    {
+        cek = Next(cek);
+        x.ID++;
+    }
+    x.ID = x.ID + 1;
     cout<<"input name song (.wav) : ";
     cin>>x.name;
     cout<<"input song location "<<endl<<"(write - for default location) :";
@@ -18,19 +28,24 @@ void inputNewSong(infotype &x)
     }
 }
 
-
 void printInfo(List L)
 {
-    address P = First(L);
-    do
+    address Q = First(L);
+    if (Q == NULL)
     {
-        cout << "nama: "<< Info(P).name << endl
-             << "Location: " << Info(P).location << endl
-             << "ID: " << Info(P).ID << endl;
-        P=Next(P);
+        cout<<"List Kosong";
     }
-    while(P!=First(L));
-
+    else
+    {
+        do
+        {
+            cout<<"name : "<<Info(Q).name<<endl
+                <<"location: "<<Info(Q).location<<endl
+                <<"ID : "<<Info(Q).ID<<endl;
+            Q = Next(Q);
+        }
+        while(Q!= First(L));
+    }
 }
 
 void playSong(address P)
@@ -41,23 +56,105 @@ void playSong(address P)
     _sleep(1000); //delay 1 second
 }
 
-void playNext(address &P){
+void playNext(address &P)
+{
     P = Next(P);
     playSong(P);
 }
 
-void playRepeat(List &l, int n)
+void playPrev(address &P)
 {
-    int a=1;
-    address p=First(l);
-    while (a<=n)
+    P = Prev(P);
+    playSong(P);
+}
+
+void playRepeat(List &L, int n)
+{
+    int x = 0,v = n;
+    address P = First(L);
+
+    do
     {
-        playSong(p);
-        p=Next(p);
-        if (p==First(l))
+        system("cls");
+        cout<<"Repeat "<<v<<" times"<<endl;
+        playSong(P);
+        P=Next(P);
+        if (P == First(L))
         {
-            a++;
+            x++;
+            v--;
         }
     }
-};
+    while (x<n);
+
+}
+void shuffleList(List &L)
+{
+    List L2;
+    address P;
+    createList(L2);
+
+    while (First(L) != NULL)
+    {
+        deleteFirst(L,P);
+        insertFirst(L2,P);
+    }
+    while (First(L2) != NULL)
+    {
+        deleteFirst(L2,P);
+        insertLast(L,P);
+    }
+}
+
+void sortList(List &L, int condition)
+{
+    address P,Q,minn;
+
+    List L2;
+    createList(L2);
+
+    while (First(L) != NULL)
+    {
+        deleteFirst(L,P);
+        insertFirst(L2,P);
+    }
+    if (condition == 1)
+     {
+         while (First(L2) != NULL)
+         {
+             P = First(L2);
+             minn = P;
+             Q = Next(P);
+             while (Q != First(L2))
+             {
+                 if (Info(Q).ID < Info(minn).ID)
+                 {
+                     minn = Q;
+                 }
+                 Q = Next(Q);
+             }
+             deletee(L2,minn);
+             insertLast(L,minn);
+         }
+     }
+     else if (condition == 2)
+     {
+          while (First(L2) != NULL)
+         {
+             P = First(L2);
+             minn = P;
+             Q = Next(P);
+             while (Q != First(L2))
+             {
+                 if (Info(Q).name < Info(minn).name)
+                 {
+                     minn = Q;
+                 }
+                 Q = Next(Q);
+             }
+             deletee(L2,minn);
+             insertLast(L,minn);
+         }
+     }
+}
 
