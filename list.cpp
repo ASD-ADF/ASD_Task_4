30744d8127be6285d6695bfe4e4bac8ffd75c4b9
sizoune <muhammadwildaniskandar@gmail.com>
2015-10-05 16:45:34 +0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -147,11 +147,11 @@ void insertAfter(List &L, address P, address Prec)
     }
     else if (Last(L) == Prec)
     {
-       Next(P) = First(L);
-       Next(Prec) = P;
-       Prev(P) = Prec;
-       Prev(First(L)) = P;
-       Last(L) = P;
+        Next(P) = First(L);
+        Next(Prec) = P;
+        Prev(P) = Prec;
+        Prev(First(L)) = P;
+        Last(L) = P;
     }
     else
     {
@@ -161,4 +161,109 @@ void insertAfter(List &L, address P, address Prec)
         Prev(P) = Prec;
     }
 
+}
+
+address findElm(List L, infotype x)
+{
+    address Q = First(L);
+
+    do
+    {
+        if(Info(Q).name == x.name)
+        {
+            return Q;
+            break;
+        }
+        Q = Next(Q);
+    }
+    while(Q!= First(L));
+    return NULL;
+}
+
+void deleteFirst(List &L, address &P)
+{
+    if (First(L) == NULL)
+    {
+        cout<<"List Kosong";
+        //getch();
+    }
+    else if (First(L) == Last(L))
+    {
+        P = First(L);
+        Next(P) = NULL;
+        Prev(P) = NULL;
+        First(L) = NULL;
+        Last(L) = NULL;
+    }
+    else
+    {
+        P = First(L);
+        First(L) = Next(P);
+        Next(Last(L)) = First(L);
+        Prev(First(L)) = Last(L);
+        Next(P) = NULL;
+        Prev(P) = NULL;
+    }
+}
+
+void deleteLast(List &L, address &P)
+{
+    if (First(L) == NULL)
+    {
+        cout<<"List Kosong";
+        //getch();
+    }
+    else if (First(L) == Last(L))
+    {
+        P = Last(L);
+        Next(P) = NULL;
+        Prev(P) = NULL;
+        First(L) = NULL;
+        Last(L) = NULL;
+    }
+    else
+    {
+        P = Last(L);
+        Last(L) = Prev(P);
+        Next(Last(L)) = First(L);
+        Prev(First(L)) = Last(L);
+        Next(P) = NULL;
+        Prev(P) = NULL;
+    }
+}
+
+void deleteAfter(List &L, address &P, address &Prec)
+{
+    if (First(L) == NULL || First(L) == Last(L))
+    {
+        cout<<"Hanya ada satu / tidak ada data";
+        //getch();
+    }
+    else
+    {
+        P = Next(Prec);
+        Next(Prec) = Next(P);
+        Prev(Next(P)) = Prec;
+        Next(P) = NULL;
+        Prev(P) = NULL;
+    }
+}
+
+void deletee(List &L, address &P)
+{
+    if (P == First(L))
+    {
+        deleteFirst(L,P);
+    }
+    else if (Next(P) == First(L))
+    {
+        deleteLast(L,P);
+    }
+    else
+    {
+        address prec = First(L);
+        while (Next(prec) != P)
+            prec = Next(prec);
+        deleteAfter(L,P,prec);
+    }
 }
