023c8d060c7b0f6e91820dcbf7cd661c98484faa
wendianto <wendianto.96@gmail.com>
2015-10-05 15:41:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 using namespace std;
 
 List L;
-address P;
+address P,Q;
 infotype x;
+int pil;
 
 void menu();
 void displayMenu();
@@ -40,7 +41,7 @@ void menu() {
         displayMenu();
         cin>>pil;
         runMenu(pil);
-    } while (pil!=5);
+    } while (pil!=10);
 }
 
 void displayMenu() {
@@ -48,10 +49,14 @@ void displayMenu() {
         <<"2. view list"<<endl
         <<"3. play first song"<<endl
         <<"4. play next "<<endl
-        <<"5. exit"<<endl;
+        <<"5. play previous song"<<endl
+        <<"6. search song by name"<<endl
+        <<"7. play again song (Last played song)"<<endl
+        <<"8. sort song by...."<<endl
+        <<"9. repeat playlist"<<endl
+        <<"10. exit"<<endl;
     cout<<"choose menu : ";
 }
-
 void runMenu(int menu) {
     switch(menu) {
     case 1 :
@@ -59,21 +64,71 @@ void runMenu(int menu) {
         inputNewSong(x);
         P = alokasi(x);
         insertFirst(L,P);
+        getch();
         break;
     case 2:
         printInfo(L);
+        getch();
         break;
     case 3 :
         P = First(L);
         playSong(P);
+        Q=P;
+        getch();
         break;
     case 4:
         playNext(P);
+        Q=P;
         break;
     case 5:
+        playPrev(P);
+        Q=P;
+        getch();
+        break;
+    case 6:
+        cout<<"Masukkan lagu yang akan dicari: "; cin>> x.name;
+        P=findElm(L,x);
+        if (P!= NULL) {
+            playSong(P);
+            Q=P;
+        }
+        else {
+            cout<<"Lagi Tidak Ditemukan"<<endl;
+        }
+        getch();
+        break;
+    case 7:
+        if (Q==NULL) {
+            cout<<"Belum ada lagu yang dimainkan"<<endl;
+
+        }
+        else {
+            playSong(Q);
+        }
+        getch();
+        break;
+    case 8:
+        cout<<"1. By ID"<<endl
+            <<"2. By Name"<<endl
+            <<"Masukkan Pilihan: "; cin>>pil;
+        if(pil<1 && pil>2) {
+            cout<<"Anda Salah Menginputkan";
+        }
+        else {
+            sortList(L,pil);
+        }
+        break;
+    case 9:
+        int n;
+        cout<<"Ingin Berapa Kali Repeat?"; cin>>n;
+        playRepeat(L,n);
+        break;
+
+    case 10:
         cout<<"thank you"<<endl;
         break;
     default :
         cout<<"wrong input"<<endl;
     }
 }
+
