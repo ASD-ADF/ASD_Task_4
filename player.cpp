6220b1f7df4c8285e7a1e9ad1fc184c4c02feb2d
NauvalWali <soraroxas.nwm@gmail.com>
2015-10-05 21:33:57 +0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -7,6 +7,16 @@
 using namespace std;
 
 void inputNewSong(infotype &x){
+    address cek;
+    x.ID = 0;
+    cek = First(L);
+    x.ID++;
+    while (cek != Last(L))
+    {
+        cek = Next(cek);
+        x.ID++;
+    }
+    x.ID = x.ID + 1;
     cout<<"input name song (.wav) : ";
     cin>>x.name;
     cout<<"input song location "<<endl<<"(write - for default location) :";
@@ -17,12 +27,19 @@ void inputNewSong(infotype &x){
 void printInfo(List L)
 {
     address Q = First(L);
-    while(Q != NULL)
+    if(Q == NULL)
     {
+        cout<<"List Tidak Berisi"
+    }
+    else
+    {
+        do
+        {
         cout<<"name : "<<Info(Q).name<<endl
             <<"location: "<<Info(Q).location<<endl;
+            <<"ID: "<<info(Q).ID<<endl;
         Q = Next(Q);
-    }
+    }while (Q!=First(L));
 }
 
 void playSong(address P){
@@ -37,3 +54,47 @@ void playNext(address &P){
     playSong(P);
 }
 
+void playPrev(address &P)
+{
+    P = Prev(P);
+    playSong(P);
+}
+
+void playRepeat(List &L, int n)
+{
+    int x = 0,v = n;
+    address P = First(L);
+
+    do
+    {
+        system("cls");
+        cout<<"Repeat "<<v<<" times"<<endl;
+        playSong(P);
+        P=Next(P);
+        if (P == First(L))
+        {
+            x++;
+            v--;
+        }
+    }
+    while (x<n);
+
+}
+
+void shuffleList(List &L)
+{
+    List L2;
+    address P;
+    createList(L2);
+
+    while (First(L) != NULL)
+    {
+        deleteFirst(L,P);
+        insertFirst(L2,P);
+    }
+    while (First(L2) != NULL)
+    {
+        deleteFirst(L2,P);
+        insertLast(L,P);
+    }
+}
