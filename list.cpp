cb04e2fad4b8f87865c2fcb8a9d69bb940ae08a7
chandragk <cgilang742@gmail.com>
2015-12-12 18:18:34 +0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void createList(List &L)
 {
     First(L) = NULL;
+    Last(L) = NULL;
 }
 
 address alokasi(infotype x)
@@ -13,6 +14,7 @@ address alokasi(infotype x)
     address P = new elemenList;
     Info(P) = x;
     Next(P) = NULL;
+    Prev(P) = NULL;
     return P;
 }
 
@@ -23,78 +25,159 @@ void dealokasi(address &P)
 
 void insertFirst(List &L, address P)
 {
-    Next(P) = First(L);
-    First(L) = P;
+    if(First(L)==NULL) {
+        First(L)=P;
+        Next(P)=First(L);
+        Prev(P)=First(L);
+        Last(L)=P;
+    }
+    else {
+        Next(P) = First(L);
+        Prev(P) = Last(L);
+        Next(Last(L))=P;
+        Prev(Last(L)) = P;
+        First(L) = P;
+    }
 }
 
 void insertLast(List &L, address P)
 {
-    if(First(L) == NULL)
-    {
+    if(First(L)==NULL){
         insertFirst(L,P);
     }
-    else
-    {
-        address Q = First(L);
-        while(Next(Q) != NULL)
-        {
-            Q = Next(Q);
-        }
-        Next(Q) = P;
+    else {
+        Next(P) = First(L);
+        Prev(P) = Last(L);
+        Next(Last(L))=P;
+        Prev(Last(L)) = P;
+        Last(L) = P;
     }
 }
 void insertAfter(List &L, address P, address Prec)
 {
-    if(First(L) == NULL)
+   if(Prec == Last(L))
     {
-        insertFirst(L,P);
+        insertLast(L,P);
     }
     else
     {
         Next(P) = Next(Prec);
+        Prev(P) = Prec;
         Next(Prec) = P;
+        Prev(Next(Prec)) = P;
     }
 }
 
-void deleteFirst(List &L, address &P)
-{
-    P = First(L);
-    First(L) = Next(P);
-    Next(P) = NULL;
+void deleteFirst(List &L, address &P){
+    if(First(L)==NULL) {
+        cout<<"Data Kosong";
+    }
+    else if (First(L)==Last(L)) {
+            P=First(L);
+            Next(P)=NULL;
+            Prev(P)=NULL;
+            First(L)=NULL;
+            Last(L)=NULL;
+    }
+    else{
+        P = First(L);
+        First(L) = Next(P);
+        Next(P) = NULL;
+        Prev(P) = NULL;
+        Prev(First(L))=Last(L);
+        Next(Last(L))=First(L);
+    }
+
 }
 
-void deleteLast(List &L, address &P)
-{
-    if(Next(First(L)) == NULL)
+void deleteLast(List &L, address &P){
+    if(First(L)==NULL)
     {
-        deleteFirst(L,P);
+        cout<<" Empty Data "<<endl;
+    }
+    else if(First(L)==Last(L)) {
+        P=Last(L);
+        Next(P)=NULL;
+        Prev(P)=NULL;
+        First(L)=NULL;
+        Last(L)=NULL;
     }
     else
     {
-        address Q = First(L);
-        while(Next(Next(Q)) != NULL)
-        {
-            Q = Next(Q);
-        }
-        P = Next(Q);
-        Next(Q) = NULL;
+        P=Last(L);
+        Last(L)=Prev(Last(L));
+        Prev(P)=NULL;
+        Next(P)=NULL;
+        Prev(First(L))=Last(L);
+        Next(Last(L))=First(L);
     }
 }
 
 void deleteAfter(List &L, address &P, address &Prec)
 {
-    P = Next(Prec);
-    Next(Prec) = Next(P);
-    Next(P) = NULL;
+   if(Prec==NULL) {
+        cout<<" Empty Data "<<endl;
+    }
+    else if (Prec==First(L)) {
+        deleteFirst(L,P);
+    }
+    else {
+     P = Next(Prec);
+     Next(Prec) = Next(P);
+     Prev(Next(P))=Prec;
+     Next(P) = NULL;
+     Prev(P) = NULL;
+    }
 }
 
-address findElm(List L, infotype x){
-    address Q = First(L);
-    while(Q != NULL){
-        if(Info(Q).ID == x.ID){
-            return Q;
-        }
-        Q = Next(Q);
+void insertID (List &L2, address P) {
+    address Q=First(L2);
+    if (Q==NULL) {
+        insertFirst(L2,P);
     }
-    return NULL;
+    else if(Info(P).ID < Info(Q).ID) {
+        insertFirst(L2,P);
+    }
+    else {
+        while (Info(P).ID > Info(Q).ID && Next(Q)!= First(L2)) {
+            if (Info(Next(Q)).ID > Info(P).ID) {
+                break;
+            }
+            Q=Next(Q);
+        }
+        insertAfter(L2,P,Q);
+    }
+}
+
+void insertName (List &L2, address P)
+{
+    address Q;
+    Q=First(L2);
+    if(Q==NULL) {
+        insertFirst(L2,P);
+    }
+     else if(Info(P).Name < Info(Q).Name) {
+        insertFirst(L2,P);
+    }
+    else {
+        while (Info(P).Name > Info(Q).Name && Next(Q)!= First(L2)) {
+            if (Info(Next(Q)).Name > Info(P).Name) {
+                break;
+}
+
+address findElm(List L, infotype x) {
+    address P = First(L);
+    while(Next(P) != First(L)){
+        if(Info(P).Name == x.Name){
+            break;
+        }
+        P = Next(P);
+    }
+    if(Info(P).Name==x.Name) {
+        return P;
+    }
+    else {
+        return NULL;
+    }
+
 }
