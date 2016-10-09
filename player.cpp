2eeb4e94047236c049fe90c610bface3ff20ffcf
IbrahimBimo <ibrahimbimo11@gmail.com>
2016-10-10 03:59:36 +0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -58,7 +58,8 @@ void playPrev(address &P){
 	* FS : P menunjuk prev lagu dan lagu dimainkan
 	*/
 	//-------------your code here-------------
-
+    P=Prev(P);
+    playSong(P);
 
     //----------------------------------------
 	
@@ -70,7 +71,11 @@ void shuffleList(List &L){
 	* FS : isi (elemen) dari list teracak
 	*/
 	//-------------your code here-------------
-
+    address P=L.first;
+    int numRand = rand()%10+1;
+    for (int i=1;i<=numRand;i++){
+        P=Next(P);
+    }
 
     //----------------------------------------	
 }
@@ -83,7 +88,74 @@ void sortList(List &L, int condition){
 	*      jika kondisi = 2, sort by nama
 	*/
 	//-------------your code here-------------
-
+    address P,Q;
+    infotype x;
+    int maxmax,batasi = 0;
+    string maxmaxs,batass = "";
+    List L2;
+    createList(L2);
+    P = L.first;
+    int counter = 0;
+    do
+    {
+         counter++;
+         P = P->next;
+    }
+    while(P!=L.first);
+ 
+    for (int i = counter; i!=0; i--)
+    {
+        if (batasi == 0 && batass == "")
+        {
+            do
+            {
+                x = P->info;
+                if (x.ID > batasi && condition == 1)
+                {
+                     batasi = x.ID;
+                     batass = x.name;
+                }
+                if (x.name > batass && condition == 2)
+                 {
+                     batasi = x.ID;
+                     batass = x.name;
+                 }
+                 P = P->next;
+             }
+             while(P!=L.first);
+             x.ID = batasi;
+             x.name = batass;
+             insertFirst(L2,alokasi(findElm(L,x)->info));
+         }
+         else
+         {
+             do
+            {
+                 x = P->info;
+                 if (x.ID > maxmax && x.ID < batasi && condition == 1)
+                 {
+                     maxmax = x.ID;
+                     maxmaxs = x.name;
+                 }
+                 if (x.name > maxmaxs && x.name < batass && condition == 2)
+                 {
+                     maxmax = x.ID;
+                     maxmaxs = x.name;
+                 }
+                 P = P->next;
+             }
+             while(P!=L.first);
+ 
+             batasi = maxmax;
+             batass = maxmaxs;
+             x.ID = batasi;
+             if (batasi != 0)
+                 insertFirst(L2,alokasi(findElm(L,x)->info));
+             maxmax = 0;
+             maxmaxs = "";
+         }
+    }
+    L = L2;
 
     //----------------------------------------	
 	
@@ -95,7 +167,12 @@ void playRepeat(List &, int n){
 	*      dari lagu pertama hingga terakhir sebanyak n kali
 	*/
 	//-------------your code here-------------
-
+    address P=L.first;
+    int totData=countElm(L);
+    playSong(P);
+    for(int i=1;i<totData*n;i++){
+        playNext(P);
+    }
 
     //----------------------------------------	
 }
@@ -108,7 +185,30 @@ void deleteSong(List &L){
 	* FS : elemen dengan ID yang dicari didealokasi
 	*/
 	//-------------your code here-------------
-
+    int number;
+ 	address P;
+    cout <<"Pilih Posisi    : 1. First"<<endl
+              <<"                  2. After"<<endl
+              <<"                  3. Last"<<endl
+              <<"Masukkan Posisi : ";cin>>number;
+        if (number == 1) {
+             deleteFirst(L,P);
+             cout << "Lagu Berhasil Dihapus";
+        }
+        else if (number == 2) {
+            address R = new elmlist;
+            cout << "Masukkan ID Element Sebelum Element Yang Akan Dihapus : ";
+            cin >> R->info.ID;
+            deleteAfter(L,P,R);
+            cout << "Lagu Berhasil Dihapus";
+        }
+        else if (number == 3) {
+            deleteLast(L,P);
+            cout << "Lagu Berhasil Dihapus";
+        }
+        else {
+            cout <<"Menu Salah"<<endl;
+        }
 
     //----------------------------------------	
 	
