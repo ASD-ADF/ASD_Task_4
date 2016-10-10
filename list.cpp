36fa2805776353d586b138055566b20b57860f5e
ridhwanalifudin <ridhwanalifudin9@gmail.com>
2016-10-10 15:50:35 +0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -4,10 +4,7 @@ void createList(List &L) {
     /**
     * FS : first(L) diset Nil
     */
-    //-------------your code here-------------
-    
-
-    //----------------------------------------
+    first(L) = Nil;
 }
 
 address alokasi(infotype x) {
@@ -16,10 +13,10 @@ address alokasi(infotype x) {
     */
 
     address P;
-    //-------------your code here-------------
-
-
-    //----------------------------------------
+    P = new ElmList;
+    info(P) = x;
+    next(P) = Nil;
+    return P;
     return P;
 }
 
@@ -27,10 +24,7 @@ void dealokasi(address &P) {
     /**
     * FS : menghapus elemen yang ditunjuk oleh P (delete)
     */
-    //-------------your code here-------------
-
-	
-    //----------------------------------------
+    delete(P);
 }
 
 void insertFirst(List &L, address P) {
@@ -38,10 +32,22 @@ void insertFirst(List &L, address P) {
     * IS : List L mungkin kosong
     * FS : elemen yang ditunjuk P menjadi elemen pertama pada List L
     */
-    //-------------your code here-------------
-	
-
-    //----------------------------------------
+    address Q;
+    if (first(L) == Nil)
+    {
+        first(L) = P;
+        next(P) = first(L);
+    }
+    else
+    {
+        while (next(Q) != first(L))
+        {
+            Q = Q->next;
+        }
+        next(P) = first(L);
+        Q->next = P;
+        first(L) = P;
+    }
 }
 
 void insertLast(List &L, address P) {
@@ -49,10 +55,22 @@ void insertLast(List &L, address P) {
     * IS : List L mungkin kosong
     * FS : elemen yang ditunjuk P menjadi elemen terakhir pada List L
     */
-    //-------------your code here-------------
-    
-	
-    //----------------------------------------
+    address Q;
+    if (first(L) == Nil)
+    {
+        first(L) = P;
+        next(P) = first(L);
+    }
+    else
+    {
+        Q = first(L);
+        while (Q->next != first(L))
+        {
+            Q = Q->next;
+        }
+        next(P) = first(L);
+        Q->next = P;
+    }
 }
 
 address findElm(List L, infotype x) {
@@ -61,12 +79,22 @@ address findElm(List L, infotype x) {
     * FS : mengembalikan elemen dengan info.ID = x.ID,
            mengembalikan Nil jika tidak ditemukan
     */
-
-    address P;
-    //-------------your code here-------------
-    
-	
-    //----------------------------------------
+    address Q;
+    if (first(L) == Nil)
+    {
+        first(L) = P;
+        next(P) = first(L);
+    }
+    else
+    {
+        Q = first(L);
+        while (Q->next != first(L))
+        {
+            Q = Q->next;
+        }
+        next(P) = first(L);
+        Q->next = P;
+    }
     return P;
 }
 
@@ -75,11 +103,24 @@ void deleteFirst(List &L, address &P) {
     * IS : List L mungkin kosong
     * FS : elemen pertama di dalam List L dilepas dan disimpan/ditunjuk oleh P
     */
-    //-------------your code here-------------
-
-	
-	
-    //----------------------------------------
+    address Q = first(L);
+    P = first(L);
+    if (next(P) == first(L))
+    {
+        P = first(L);
+        next(P) = Nil;
+        first(L) = Nil;
+    }
+    else
+    {
+        while (next(Q)!=first(L))
+        {
+            Q = next(Q);
+        }
+        next(Q)=next(P);
+        first(L)=next(P);
+        next(P)=Nil;
+    }
 }
 
 void deleteLast(List &L, address &P) {
@@ -87,11 +128,24 @@ void deleteLast(List &L, address &P) {
     * IS : List L mungkin kosong
     * FS : elemen tarakhir di dalam List L dilepas dan disimpan/ditunjuk oleh P
     */
-    //-------------your code here-------------
+    address Q = first(L);
+    if (next(first(L)) == Nil)
+    {
+        P = first(L);
+        next(P) = Nil;
+        first(L) = Nil;
+    }
+    else
+    {
+        while (next(next(Q))!=first(L))
+        {
+            Q=next(Q);
+        }
 
-	
+        next(Q) = first(L);
+        next(P) = Nil;
 
-    //----------------------------------------
+    }
 }
 
 void insertAfter(address Prec, address P) {
@@ -100,10 +154,8 @@ void insertAfter(address Prec, address P) {
     * FS : elemen yang ditunjuk P menjadi elemen di belakang elemen yang
     *      ditunjuk pointer Prec
     */
-    //-------------your code here-------------
-
-	
-    //----------------------------------------
+    next(P) = next(Prec);
+    next(Prec) = P;
 
 }
 void deleteAfter(address Prec, address &P) {
@@ -112,9 +164,18 @@ void deleteAfter(address Prec, address &P) {
     * FS : elemen yang berada di belakang elemen Prec dilepas
     *      dan disimpan/ditunjuk oleh P
     */
-    //-------------your code here-------------
-    
-	
-    //----------------------------------------
+    if (next(Prec)==first(L))
+    {
+        P=first(L);
+        first(L)=next(P);
+        next(Prec)=first(L);
+        P=Nil;
+    }
+    else
+    {
+        P = next(Prec);
+        next(Prec) = next(P);
+        next(P) = Nil;
+    }
 }
 
