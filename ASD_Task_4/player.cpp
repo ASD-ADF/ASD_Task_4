3c05ac8afd1c41c9db5ec3b052c4782504eb822b
Mohammaddaffaharis <46914307+Mohammaddaffaharis@users.noreply.github.com>
2019-03-04 20:04:48 +0700

    Add files via upload

---
+++ ASD_Task_4/player.cpp
@@ -0,0 +1,105 @@
+#include "player.h"
+#include <ctime>
+
+int randomInt(int  max_int) {
+    /** YOU DON'T NEED TO MODIFY THIS */
+    srand(time(NULL));
+    return (rand() % max_int) + 1;
+}
+
+void printInfo(List L) {
+    /**
+    * PR : menampilkan informasi ID, nama, dan lokasi file
+    * YOU DON'T NEED TO MODIFY THIS
+    */
+
+    address Q = first(L);
+    do {
+        cout<<"name    : "<<info(Q).name<<endl
+            <<"ID      : "<<info(Q).ID<<endl
+            <<"location: "<<info(Q).location<<endl;
+        Q = next(Q);
+    } while(Q!=first(L));
+    cout<<"==============================================="<<endl;
+}
+
+
+void playMusic(address P) {
+    /**
+    * PR : memainkan lagu yang ditunjuk oleh pointer P
+    * YOU DON'T NEED TO MODIFY THIS
+    */
+
+    string filename = info(P).location+"/"+info(P).name;
+    cout<<"playing "<<filename<<endl;
+    PlaySound(TEXT(filename.c_str()), NULL, SND_FILENAME);
+    _sleep(500); //delay 0.5 second
+}
+
+
+void shuffleList(List &L) {
+    /**
+    * PR : mengacak isi (elemen) dari list L
+    * FS : isi (elemen) dari list teracak
+    */
+    //------------- YOUR CODE HERE -------------
+
+        cout<<"UNDER MAIN TENIS"<<endl;
+    int r = randomInt(20);
+    int i,j;
+    address P = first(L);
+    for(i=0; i < r; i++){
+        swap(info(P).name, info(next(P)).name);
+        swap(info(P).ID, info(next(P)).ID);
+        P = next(P);
+    };
+    //----------------------------------------
+}
+
+void playRepeat(List &L, int n) {
+    /**
+    * PR : memainkan seluruh lagu di dalam list
+    *      dari lagu pertama hingga terakhir sebanyak n kali
+    */
+    //------------- YOUR CODE HERE -------------
+
+        cout<<"UNDER MAIN TENIS"<<endl;
+    address P = first(L);
+    int i = 0;
+    while (i < n){
+        playMusic(P);
+        P = next(P);
+        while (P != first(L)){
+            playMusic(P);
+            P = next(P);
+        }
+        i++;
+    }
+    //----------------------------------------
+}
+
+void deleteMusicByID(List &L, infotype x) {
+    /**
+    * IS : list L mungkin kosong
+    * PR : menerima input user untuk ID lagu yang ingin dihapus
+    *      jika ID lagu ditemukan, hapus (deallocate) lagu dari list
+    * FS : elemen dengan ID yang dicari dideallocate
+    */
+    //------------- YOUR CODE HERE -------------
+
+        cout<<"UNDER MAIN TENIS"<<endl;
+    address P = first(L);
+    if (x.ID != info(P).ID){
+        P = next(P);
+    }
+    while (x.ID != info(P).ID && P != first(L)){
+        P = next(P);
+    }
+    if (x.ID == info(P).ID){
+        next(prev(P)) = next(P);
+        prev(next(P)) = prev(P);
+        deallocate(P);
+    }
+    //----------------------------------------
+
+}
---
+++ ASD_Task_4/player.h
@@ -0,0 +1,30 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+#include "list.h"
+
+/** YOU DON'T NEED TO MODIFY THESE **/
+
+int randomInt(int);
+// helper random for shuffle
+
+void printInfo(List);
+// output the songs in the list
+
+void playMusic(address P);
+// play song from element P
+
+void shuffleList(List &);
+// shuffle the song list
+
+void playRepeat(List &, int n);
+// play the song list from the first song
+// and repeat the list n times
+
+void deleteMusicByID(List &, infotype);
+// user input song ID
+// remove the song from the list
+
+
+
+#endif // PLAYER_H_INCLUDED
