08efe6dff8355666d00c7d1aea066677c66039a6
Azizah Cahya Kemila <zellchan07@gmail.com>
2020-03-07 15:56:50 +0700

    PR FROM AZIZAH CAHYA KEMILA_1301194103_IF-43-05
    Saya telah mengerjakan tugas ASD_Task_4

--- list.cpp
+++ list.cpp
@@ -5,7 +5,7 @@ void createList(List &L) {
     * FS : first(L) diset Nil
     */
     //------------- YOUR CODE HERE -------------
-
+    first(L) = NULL;
     //----------------------------------------
 }
 
@@ -17,7 +17,10 @@ address allocate(infotype x) {
 
     address P = NULL;
     //------------- YOUR CODE HERE -------------
-
+    P = new elmlist;
+    info(P).ID = x.ID;
+    info(P).location = x.location;
+    info(P).name = x.name;
     //----------------------------------------
     return P;
 }
@@ -27,7 +30,7 @@ void deallocate(address &P) {
     * FS : menghapus elemen yang ditunjuk oleh P (delete)
     */
     //------------- YOUR CODE HERE -------------
-
+    delete P;
     //----------------------------------------
 }
 
@@ -37,7 +40,17 @@ void insertFirst(List &L, address P) {
     * FS : elemen yang ditunjuk P menjadi elemen pertama pada List L
     */
     //------------- YOUR CODE HERE -------------
-
+    if (first(L) == NULL){
+        first(L) = P;
+        next(first(L)) = P;
+        prev(first(L)) = P;
+    }else{
+        next(P) = first(L);
+        next(prev(first(L))) = P;
+        prev(P) = prev(first(L));
+        prev(first(L)) = P;
+        first(L) = P;
+     }
     //----------------------------------------
 }
 
@@ -47,7 +60,17 @@ void insertLast(List &L, address P) {
     * FS : elemen yang ditunjuk P menjadi elemen terakhir pada List L
     */
     //------------- YOUR CODE HERE -------------
+    if(first(L) == NULL){
+        first(L) = P;
+        next(first(L)) = P;
+        prev(first(L)) = P;
+    }else {
+        next(P) = first(L);
+        prev(P) = prev(first(L));
+        next(prev(first(L))) = P;
+        prev(first(L)) = P;
 
+    }
     //----------------------------------------
 }
 
@@ -60,7 +83,18 @@ address findElmByID(List L, infotype x) {
 
     address P = NULL;
     //------------- YOUR CODE HERE -------------
-
+    if(first(L) == NULL){
+        cout<<"List Kosong"<<endl;
+    }else{
+        P = first(L);
+        do{
+            if(info(P).ID == x.ID){
+                return P;
+            }
+            P = next(P);
+        }while(P != first(L));
+        P = NULL;
+    }
     //----------------------------------------
     return P;
 }
@@ -74,7 +108,18 @@ address findElmByName(List L, infotype x) {
 
     address P = NULL;
     //------------- YOUR CODE HERE -------------
-
+    if(first(L) == NULL){
+        cout<<"List Kosong"<<endl;
+    }else{
+        P = first(L);
+        do{
+            if(info(P).name == x.name){
+                return P;
+            }
+            P = next(P);
+        }while(P != first(L));
+        P = NULL;
+    }
     //----------------------------------------
     return P;
 }
@@ -85,7 +130,20 @@ void deleteFirst(List &L, address &P) {
     * FS : elemen pertama di dalam List L dilepas dan disimpan/ditunjuk oleh P
     */
     //------------- YOUR CODE HERE -------------
-
+    P = first(L);
+    if(first(L) == NULL){
+        cout<<" "<<endl;
+    }else if(next(first(L) == first(L))){
+        next(P) = NULL;
+        prev(P) = NULL;
+        first(L) = NULL;
+    }else{
+        first(L) = next(P);
+        next(prev(P)) = first(L);
+        prev(first(L)) = prev(P);
+        next(P) = NULL;
+        prev(P) = NULL;
+    }
     //----------------------------------------
 }
 
@@ -95,7 +153,20 @@ void deleteLast(List &L, address &P) {
     * FS : elemen tarakhir di dalam List L dilepas dan disimpan/ditunjuk oleh P
     */
     //------------- YOUR CODE HERE -------------
-
+    P = first(L);
+    if(first(L) == NULL){
+        cout<<" "<<endl;
+    }else if(next(first(L)) == NULL){
+        first(L) = NULL;
+        next(P) = NULL;
+        prev(P) = NULL;
+    }else{
+        P = prev(P);
+        next(prev(P)) = first(L);
+        prev(first(L)) = prev(P);
+        next(P) = NULL;
+        prev(P) = NULL;
+    }
     //----------------------------------------
 }
 
@@ -106,7 +177,10 @@ void insertAfter(List &L, address &Prec, address P) {
     *      ditunjuk pointer Prec
     */
     //------------- YOUR CODE HERE -------------
-
+    next(Prec) = next(P);
+    prev(Prec) = P;
+    prev(next(P)) = Prec;
+    next(P) = Prec;
     //----------------------------------------
 
 }
@@ -117,7 +191,24 @@ void deleteAfter(List &L, address &Prec, address &P) {
     *      dan disimpan/ditunjuk oleh P
     */
     //------------- YOUR CODE HERE -------------
-
+    if(Prec == first(L) && next(Prec) == first(L)){
+        first(L) = NULL;
+        next(Prec) = NULL;
+        prev(Prec) = NULL;
+    }else if(next(Prec) == first(L)){
+        P = first(L);
+        next(Prec) = next(P);
+        prev(next(P)) = Prec;
+        first(L) = next(P);
+        next(P) = NULL;
+        prev(P) = NULL;
+    }else{
+        P = next(Prec);
+        next(Prec) = next(P);
+        prev(next(P)) = Prec;
+        next(P) = NULL;
+        prev(P) = NULL;
+    }
     //----------------------------------------
 }
 
--- main.cpp
+++ main.cpp
@@ -122,9 +122,8 @@ void runMenu(int menu) {
     case 2:
         // insert last music
         //------------- YOUR CODE HERE -------------
-        cout<<"UNDER MAIN TENIS"<<endl;
-        //input music
-        //insertLast()
+        P = inputMusic();
+        insertLast(L, P);
 
         //----------------------------------------
         cout<<"press enter";getche();
@@ -142,7 +141,8 @@ void runMenu(int menu) {
     case 5:
         // play last music
         //------------- YOUR CODE HERE -------------
-        cout<<"UNDER MAIN TENIS"<<endl;
+        P = prev(first(L));
+        playMusic(P);
 
         //----------------------------------------
         break;
@@ -154,6 +154,8 @@ void runMenu(int menu) {
         P = findElmByName(L, x);
         if(P != NULL){
             cout<<"music found"<<endl;
+        }else{
+            cout<<"music not found"<<endl;
         }
         //----------------------------------------
         cout<<"press enter";getche();
@@ -161,7 +163,14 @@ void runMenu(int menu) {
     case 7:
         // search music by ID
         //------------- YOUR CODE HERE -------------
-        cout<<"UNDER MAIN TENIS"<<endl;
+        cout<<"input music id : ";
+        cin>>x.ID;
+        P = findElmByID(L, x);
+        if(P != NULL){
+            cout<<"music found"<<endl;
+        }else{
+            cout<<"music not found"<<endl;
+        }
 
         //----------------------------------------
         cout<<"press enter";getche();
@@ -182,8 +191,10 @@ void runMenu(int menu) {
     case 10:
         // play previous music
         //------------- YOUR CODE HERE -------------
-        cout<<"UNDER MAIN TENIS"<<endl;
-
+        if(P != NULL){
+            P = prev(P);
+            playMusic(P);
+            }
         //----------------------------------------
         break;
     case 11:
@@ -202,7 +213,7 @@ void runMenu(int menu) {
     case 13:
         // delete music by ID
         cout<<"input music ID : ";
-        cin>>x.name;
+        cin>>x.ID;
         deleteMusicByID(L, x);
         cout<<"press enter";getche();
         break;
